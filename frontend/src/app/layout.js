ed2387f8a94ebf175beb6f3187b97802af41b100
Aniks18 <ak7525@srmist.edu.in>
2025-12-03 20:06:53 +0530

    majors setup

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Prime Trade",
+  description: "Scalable Web App with Authentication & Dashboard",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
